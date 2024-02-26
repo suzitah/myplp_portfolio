06b1a620d90056e73d32dd2d396b87d52be3559f
suzitah <sthiari@gmail.com>
2024-02-26 12:55:22 +0300

    first python file

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name =input("Please enter your name: ");
+age = input("Please enter your age: ");
+location = input("Please enter your location: ") ;
+
+print("Hi",name ,"Your age is", age, "and you stay at", location);
